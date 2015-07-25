bbe32cc9adb70a1432f21120cb9d614a4a79a440
dart14 <daryl.largoza@gmail.com>
2015-07-25 20:37:58 +0800

    delete unused files

--- scripts/main/app.js
+++ scripts/main/app.js
@@ -2,7 +2,19 @@
     'use strict';
 
     angular.module('app', [
-    ]);
+        'ngRoute',
+        'mobile-angular-ui'
+    ]).config(conf);
+
+    conf.$inject = ['$routeProvider'];
+
+    function conf(routeProvider) {
+        routeProvider
+             .when('/', {
+                 templateUrl: 'scripts/home/home.html'
+             });
+
+    };
 
     console.info("Refactored app module loaded.");
 })();
